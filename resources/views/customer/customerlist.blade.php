9596fa51a371b9c3b71fdba701e330c195bde7d5
iGeraldino <geraldalvaran@yahoo.com>
2017-10-08 22:33:19 +0800

    Ayan nabukod ko na yung may existing at non existing

--- resources/views/customer/customerlist.blade.php
+++ resources/views/customer/customerlist.blade.php
@@ -135,7 +135,7 @@
             <div class="nav-tabs-navigation">
               <div class="nav-tabs-wrapper">
                 <ul class="nav nav-tabs" data-tabs="tabs">
-                  <li class="active">
+                  <li  class="active">
                     <a href="#withaccount" data-toggle="tab">
                       <i class="material-icons">assignment_return</i>
                       Customer with Account
@@ -153,7 +153,152 @@
           </div>
           <div class="content">
             <div class="tab-content text-center">
-              <div class="tab-pane active" id="withaccount">
+              <div class="tab-pane  active" id="withaccount">
+                <div class="col-xs-12">
+                  <div class="box">
+                    <div class="box-header">
+                    </div>
+
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                      <table id="withaccts" class="table table-bordered table-striped">
+                        <thead style="color: #6e48aa">
+                            <th class="text-center"> ID </th>
+                            <th class="text-center"> NAME </th>
+                            <th class="text-center"> TYPE </th>
+                            <th class="text-center"> PHONE NUMBER</th>
+                            <th class="text-center"> EMAIL ADDRESS</th>
+                            <th class="text-center"> ADDRESS</th>
+                            <th class="text-center"> ACTION</th>
+                        </thead>
+
+                        <tbody>
+                          @foreach($accts as $customerDetailsrow)
+                            <tr>
+                              <td> CUST-{{$customerDetailsrow->Cust_ID}}     </td>
+                              <td> {{$customerDetailsrow->Cust_FName}} {{$customerDetailsrow->Cust_MName}}, {{$customerDetailsrow->Cust_LName}} </td>
+                              <td>
+                                <?php
+                                  if($customerDetailsrow->Customer_Type == 'C'){
+                                    echo 'Single';
+                                  }
+                                  else if($customerDetailsrow->Customer_Type == 'H'){
+                                    echo 'Hotel';
+                                  }
+                                  else if($customerDetailsrow->Customer_Type == 'S'){
+                                    echo 'Shop';
+                                  }
+                                  else{
+                                    echo 'n\a';
+                                  }
+                                ?>
+                              </td>
+                              <td> {{$customerDetailsrow->Contact_Num}}       </td>
+                              <td> {{$customerDetailsrow->Email_Address}}       </td>
+                              <td>    {{$customerDetailsrow->Address_Line}}  </td>
+                              <td align="center" >
+                                <button type="button" rel="tooltip" title="VIEW" class="btn Subu btn-sm btn-just-icon" data-toggle="modal" data-target="#viewCust{{$customerDetailsrow->Cust_ID}}"><i class="material-icons">search</i></button>
+                                <!-- line modal -->
+                                <div class="modal fade" id="viewCust{{$customerDetailsrow->Cust_ID}}" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <button type="button" class="close" rel="tooltip" title="close"
+                                        id = "closeXBtn" name = "closeXBtn" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only"><i class="material-icons">delete</i></span></button>
+                                        <h3 class="modal-title" id="lineModalLabel">Customer's Details</h3>
+                                      </div>
+                                      <div class="modal-body" id = "infoBodyDiv">
+                                        <!-- content goes here -->
+                                        <h5>
+                                          <bold>Customer ID:</bold>
+                                            CUST-{{$customerDetailsrow->Cust_ID}}
+                                        </h5>
+                                        <h5>
+                                          <bold>Customer Name:</bold>
+                                          {{$customerDetailsrow->Cust_FName}} {{$customerDetailsrow->Cust_MName}}, {{$customerDetailsrow->Cust_LName}}
+                                        </h5>
+                                        <h5>
+                                          Type: <?php
+                                            if($customerDetailsrow->Customer_Type == 'C'){
+                                              echo 'Single';
+                                            }
+                                            else if($customerDetailsrow->Customer_Type == 'H'){
+                                              echo 'Hotel';
+                                            }
+                                            else if($customerDetailsrow->Customer_Type == 'S'){
+                                              echo 'Shop';
+                                            }
+                                            else{
+                                              echo 'n\a';
+                                            }
+                                          ?>
+                                        </h5>
+                                        <h5>
+                                         Contact Number: {{$customerDetailsrow->Contact_Num}}
+                                        </h5>
+
+                                        <h5>
+                                         Email Address: {{$customerDetailsrow->Email_Address}}
+                                        </h5>
+
+                                        <h5>
+                                         Address: {{$customerDetailsrow->Address_Line}}
+                                        </h5>
+                                        <h5>
+                                         Hotel Name:
+                                         <?php
+                                          if($customerDetailsrow->Hotel_Name == " " or $customerDetailsrow->Hotel_Name == NULL){
+                                            echo 'Not aviailable';
+                                          }
+                                          else{
+                                            echo $customerDetailsrow->Hotel_Name;
+                                          }
+                                         ?>
+                                        </h5>
+                                        <h5>
+                                         Shop Name:
+                                         <?php
+                                          if($customerDetailsrow->Shop_Name == " " or $customerDetailsrow->Shop_Name == NULL){
+                                            echo 'Not aviailable';
+                                          }
+                                          else{
+                                            echo $customerDetailsrow->Hotel_Name;
+                                          }
+                                         ?>
+                                        </h5>
+                                      </div>
+                                      <div class="modal-footer" id = "infoFooter">
+                                        <div class="btn-group " role="group" aria-label="group button">
+                                          <div class="btn-group" role="group">
+                                            <button type="button" class="btn btn-danger btn-simple" data-dismiss="modal"  role="button">Close</button>
+                                          </div>
+                                          <div class="btn-group" role="group">
+                                             <a type = "button" href="{{ route('customers.edit',$customerDetailsrow->Cust_ID) }}" class = "btn   btn-success btn-simple">
+                                                Edit Details
+                                              </a>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                <a type = "button" href="{{ route('customersTradeAgreement.show',$customerDetailsrow->Cust_ID) }}" class = "btn btn-sm Inbox btn-just-icon" rel="tooltip" title="ADD TRADE AGREEMENT" >
+                                 <i class="material-icons">add_circle</i>
+                                </a>
+                              </td>
+                            </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
+                    </div>
+                    <!-- /.box-body -->
+                  </div>
+                  <!-- /.box -->
+                </div>
+                <!-- /.col -->
+
+              </div>
+              <div class="tab-pane" id="withoutaccount">
                 <div class="col-xs-12">
                   <div class="box">
                     <div class="box-header">
@@ -297,9 +442,7 @@
                   <!-- /.box -->
                 </div>
                 <!-- /.col -->
-     
-              </div>
-              <div class="tab-pane" id="withoutaccount">
+
               </div>
             </div>
           </div>
